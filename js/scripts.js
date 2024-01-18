6950e2e8e1a2e666da556b41211ca6981b1dfe05
Thais-Guimaraes <thaisbeguima@gmail.com>
2024-01-18 20:10:27 -0300

    update layout site

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', event => {
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#mainNav',
-            rootMargin: '0px 0px -40%',
+            rootMargin: '-38% 0px -38% 0px',
         });
     };
 
@@ -52,3 +52,7 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+
+
